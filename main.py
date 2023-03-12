8596215ae08d6273566829f254248cb0352453a1
Parinya(Petong) <90907279+ParinyaMoontongdang@users.noreply.github.com>
2023-03-12 16:05:08 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.camera_video
+        frame = camera.calculateAngle
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame
                + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(calculateAngle()),
+    return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
